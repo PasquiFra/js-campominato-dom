9d50b8d202142940f2873810d62947483ecf34c1
Francesco <francesco.pasquinoni@hotmail.it>
2024-01-11 19:13:56 +0100

    done till milestone #3

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-console.log("finora TUTTO OK");
+console.log("functions.js OK");
 
 const generateGrid = (content) => {
     const cell = document.createElement("div");
@@ -7,6 +7,36 @@ const generateGrid = (content) => {
     return cell;
 }
 
+const generateBomb = (maxNum, totalNums , array) => {
 
-
+    //genero un numero da 1 a totalCells
     
+    while (array.length < totalNums) {
+        let number = Math.floor(Math.random() * maxNum) + 1;
+        if (!array.includes(number)) array.push(number)
+    };
+}
+/*
+const selectAll = (array, maxNum, cell) => {
+    
+    for (cell of cells){
+        cell.classList.add("clicked");
+        if (array.includes(i)){
+            cell.classList.add("bomb");
+        }
+    }
+}
+*/
+
+const endGame = (score, hasWon = false ) => {
+    
+    const message = hasWon 
+    ? "hai vinto" 
+    : `hai perso! hai totalizzato ${score} punti"`
+    
+    cell.classList.add("bomb");
+
+    // selectAll(array, maxNum, cell)
+    
+
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("JS OK");
+console.log("script.js OK");
 
 // prelevo elementi del DOM 
 
@@ -6,20 +6,30 @@ const difficultyChoice = document.getElementById("difficulty");
 const startButton = document.getElementById("start");
 const grid = document.getElementById("grid");
 const title = document.getElementById ("begin-title");
+const points = document.getElementById ("score"); 
 
 //imposto la variabile per il num di celle per riga/colonna
 let cellsNum;
-let cellSize;
 
-startButton.addEventListener ("click", function() {
+// imposto il numero di bombe da posizionare
+const bombsToPlace = 16;
+
+// inserisco i messaggi di fine gioco 
+const loseMessage = "Hai pestato una bomba!";
+
+// imposto il contatore di punteggio
+let score = 0;
+
+function startGame () {
     
     //faccio sparire il testo
     title.classList.add = "d-none";
-
-    //se avevo già cliccato il bottone svuoto il contenuto precedentemente inserito
-    grid.innerHTML = "";
+    
+    //cliccando di nuovo il bottone svuoto i campi:
+    const generatedBombs = [];
+    grid.innerText = "";
     cellsNum = "";
-    startButton.innerHTML = "Ricomincia"
+    startButton.innerHTML = "Ricomincia";
     
     console.log("cliccato");
     
@@ -28,24 +38,23 @@ startButton.addEventListener ("click", function() {
     
     if (difficulty === "easy") {
         cellsNum = 7;
-        cellSize = " " + difficulty;
     } else if (difficulty === "medium") {
         cellsNum = 9;
-        cellSize = ` ${difficulty}`;
     } else {
         cellsNum = 10;
-        cellSize = ` ${difficulty}`;
     }
     
     const root = document.querySelector (":root");
     root.style.setProperty("--cols-per-row", cellsNum);
-
-
+    
+    
     // definisco la variabile per le rige e per le colonne
     const rows = cellsNum;
     const cols = cellsNum;
-    const totalCells = rows * cols;
-
+    let totalCells = rows * cols;
+    
+    // imposto il punteggio massimo
+    const maxPoints = totalCells - bombsToPlace ;
     console.log("numero celle (riga/colonna)", cellsNum, "totale celle", totalCells);
     
     // Genero la griglia con il numero di celle richieste
@@ -54,14 +63,32 @@ startButton.addEventListener ("click", function() {
         //inserisco la funzione che genera una singola griglia
         const cell = generateGrid(i);
         
+        //inserisco la funzione di generazione delle bombe
+        generateBomb (totalCells, bombsToPlace, generatedBombs);
+        
         //inserisco l'event listener che mi permette di applicare uno sfondo quando clicco
         cell.addEventListener("click", function() {
 
             if (cell.classList.contains("clicked")) return;
+            
+            cell.classList.add("clicked");
+            console.log("i: ", i);
 
-            cell.classList.toggle("clicked");
-            console.log("cell.innerText :", cell.innerText)
-            console.log("i: ", i)
+            // controllo se ho cliccato una bomba
+            const HitBomb = generatedBombs.includes(i);
+
+            if (HitBomb) {
+                console.log(loseMessage);
+                cell.classList.add("bomb");
+                endGame(score, false);
+            } else {
+                score += 1;
+                points.innerText = score;
+
+                if (score === maxPoints) {
+                    endGame(score, true)
+                }
+            }
 
         })
         
@@ -70,4 +97,8 @@ startButton.addEventListener ("click", function() {
         grid.appendChild(cell);
     }
     
-})
+    console.log("generated bombs: ", generatedBombs);
+}
+
+startButton.addEventListener ("click", startGame);
+
